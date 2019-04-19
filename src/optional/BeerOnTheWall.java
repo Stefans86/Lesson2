53af4c8f86bbb3da58c7ab3dba9ea2ee6c0cdad8
unknown <you@example.com>
2019-04-19 22:42:15 +0200

    First commit

--- src/optional/BeerOnTheWall.java
+++ src/optional/BeerOnTheWall.java
@@ -1,10 +1,6 @@
 package optional;
 
-/* 
- * Print the lyrics to the song "99 Bottles of Beer on the wall"
- * 
- * http://99-bottles-of-beer.net/lyrics.html
- */
+
 
 public class BeerOnTheWall {
 	public static void main(String[] args) {

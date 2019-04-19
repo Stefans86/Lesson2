106c211c97a71cb5edd0de2799a46e07b7ca4a85
unknown <you@example.com>
2019-04-19 22:35:17 +0200

    First commit

--- src/exercises/NASACountdown.java
+++ src/exercises/NASACountdown.java
@@ -2,10 +2,20 @@ package exercises;
 
 /*
  * Count down a rocket launch!
- * 
- * Print the numbers from 10 to 1, then print “blastoff”. 
+ 
+ * Print the numbers from 10 to 1, then print â€œblastoffâ€�. 
  */
 
 public class NASACountdown {
+	public static void main(String[] args) {
 
+		System.out.println("Counting down, starting in ");
+
+		for (int i = 10; i > 0; i--) {
+
+			System.out.println(i);
+
+		}
+		System.out.println("Blastoff !!! weeeeeeeeee !!! ");
+	}
 }
